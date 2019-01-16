6ba73dcbfabb1f0ec9afa7103cd9db6d94fd8e0d
bbcPhoebe <31885279+bbcPhoebe@users.noreply.github.com>
2019-01-16 12:37:42 -0500

    Package (#8)
    * update package.json

--- src/connectAnnouncements.js
+++ src/connectAnnouncements.js
@@ -2,6 +2,22 @@ import React from 'react';
 
 const AnnounceMessageContext = React.createContext();
 
+// const connectAriaAnnouncementProvider = (Section) => {
+//   function ConnectedAriaLiveAnnouncements({ announce, ariaMessages, ...props }) {
+//     function triggerAnnouncement(action, ...args) {
+//       const message = ariaMessages[action](...args)
+//       return announce(message, Date.now())
+//     }
+
+//     return (
+//       <AnnounceMessageContext.Provider value={triggerAnnouncement} > 
+//         <Section {...props} />
+//       </AnnounceMessageContext.Provider> 
+//     )
+//   }
+//   return ConnectedAriaLiveAnnouncements;
+// }
+
 const connectAriaAnnouncementProvider = (Section) => {
   const ConnectedAriaLiveAnnouncements = React.forwardRef(({ announce, ariaMessages, ...props }, ref) => {
     function triggerAnnouncement(action, ...args) {
@@ -17,6 +33,22 @@ const connectAriaAnnouncementProvider = (Section) => {
   })
   return ConnectedAriaLiveAnnouncements;
 }
+
+const Testing = React.forwardRef(({ announce, ariaMessages, ...props }, ref) => {
+
+  function triggerAnnouncement(action, ...args) {
+    const cb = args.pop();
+    console.log(cb, args)
+    const message = ariaMessages[action](...args)
+    return announce(message, Date.now(), cb)
+  }
+
+  return (
+    <AnnounceMessageContext.Provider value={triggerAnnouncement} > 
+      <Section {...props} ref={ref} />
+    </AnnounceMessageContext.Provider> 
+  )
+})
 
 const connectAriaAnnouncementConsumer = (Component) => {
   function ConnectedAriaAnnouncer(props) {
